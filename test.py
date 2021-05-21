5459e588efc9334c39f8d368af41647f5926416a
00tarunkumar <84559726+00tarunkumar@users.noreply.github.com>
2021-05-21 11:48:37 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+df=pd.read_csv("C:\\Users\\Arun kumar\\Desktop\\year.csv")
+#print(df.head())
+
+
+for i in range(9):
+    df["prices"]=456
+    df.to_csv("C:\\Users\\Arun kumar\\Desktop\\year.csv",index=False)
+
+
+
+
+#df["prices"]=456
+#df.to_csv("C:\\Users\\Arun kumar\\Desktop\\year.csv",index=False)
+print(df.to_string())
